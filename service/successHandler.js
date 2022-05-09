12b420425cffe42b15179d79f32cda5eed961b20
MoriHp <moriko232@gmail.com>
2022-05-09 21:19:33 +0800

    參考第三周架構講解: 已有cors套件，可以刪掉header，簡化succcessHandler

--- service/successHandler.js
+++ service/successHandler.js
@@ -1,13 +1,8 @@
-const headers = require("./headerSetting.js");
-
 function successHandler(res, data = "") {
-  res.writeHead(200, headers);
-  res.write(
-    JSON.stringify({
-      status: "success",
-      data,
-    })
-  );
+  res.send({
+    status: "success",
+    data,
+  });
   res.end();
 }
 
