a612f3a4255a780707096250d863aed0aa8c65d2
MoriHp <moriko232@gmail.com>
2022-05-14 17:20:18 +0800

    [Func] 註冊會員API

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,65 @@
 var express = require("express");
+var validator = require("validator");
+var bcrypt = require("bcrypt");
 var router = express.Router();
 const User = require("../model/userModel.js");
 
+const generateSendJWT = require("../service/generateSendJWT.js");
 const appError = require("../service/appError");
 const handleErrAsync = require("../service/handleErrAsync");
 const successHandler = require("../service/successHandler");
 
+// 列出會員
+router.get(
+  "/users",
+  handleErrAsync(async (req, res, next) => {
+    const allUser = await User.find();
+    successHandler(res, allUser);
+  })
+);
+
+// 註冊會員
 router.post(
   "/user/sign_up",
   handleErrAsync(async (req, res, next) => {
     const data = req.body;
+    let { email, userName, password } = data;
 
-    // 檢查欄位
-    if (data.userName === "") {
-      appError({ errMessage: "未填寫userName" }, next);
+    // 手動檢查欄位
+    if (!userName) {
+      return appError({ errMessage: "未填寫userName" }, next);
+    }
+    if (!email) {
+      return appError({ errMessage: "未填寫email" }, next);
+    }
+    if (!password) {
+      return appError({ errMessage: "未填寫password" }, next);
+    }
+    if (!validator.isEmail(email)) {
+      return appError({ errMessage: "email格式有誤" }, next);
+    }
+    if (!validator.isLength(password, { min: 8 })) {
+      return appError({ errMessage: "密碼未達8碼" }, next);
+    }
+    const existUser = await User.find({ email });
+    if (existUser.length) {
+      return appError({ errMessage: "此email已被使用" }, next);
     }
 
+    // 加密password
+    password = await bcrypt.hash(password, 12);
+    console.log("new pw", password);
+
     const user = {
-      userName: data.userName,
-      email: data.email,
-      password: data.password,
+      userName,
+      email,
+      password,
     };
-    await User.create(user).then(async () => {
-      const allUser = await User.find();
-      successHandler(res, allUser);
-    });
+    console.log("new user", user);
+
+    const newUser = await User.create(user);
+
+    generateSendJWT(newUser, 201, res);
   })
 );
 
