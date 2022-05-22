867af488f9bfd00e1a6294fc4f75ea159847cf91
MoriHp <moriko232@gmail.com>
2022-05-22 16:20:34 +0800

    fix bug - find correct user

--- service/isAuth.js
+++ service/isAuth.js
@@ -31,7 +31,7 @@ async function isAuth(req, res, next) {
   });
   // console.log("decodeJWT", decodeJWT);
 
-  const currentUser = await User.findOne({ id: decodeJWT.id });
+  const currentUser = await User.findOne({ _id: decodeJWT.id });
   // console.log("currentUser", currentUser);
   
   // 將通過的user寫到req再傳回
